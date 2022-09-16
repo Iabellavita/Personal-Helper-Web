71c4e4a3c99225207f1508933a866dce5773a11e
Iabellavita <vladganggg@gmail.com>
2022-09-16 13:59:58 +0300

    something changes and news

---
+++ contacts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('create_contact/', create_contact, name='create_contact'),
+    path('contacts/', ContactByUser.as_view(), name='contact_list'),
+    path('contact/<int:pk>', user_contact, name='contact'),
+    path('search_contact/', Search.as_view(), name='search_contact'),
+]
