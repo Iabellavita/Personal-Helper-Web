8c9dd75cd59edeff2181726200c1052dbb33cb14
Korets Roman <vip.korets@ukr.net>
2022-09-18 19:24:14 +0300

    add delete list to file

--- files/views.py
+++ files/views.py
@@ -11,11 +11,27 @@ import os
 
 class FilesByUser(ListView):
     model = Files
+    initial = {'key': 'value'}
     template_name = 'files/files_view.html'
     context_object_name = 'files_data'
 
     def get_queryset(self):
         return Files.objects.filter(user=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        file_list = self.request.POST.getlist('file')
+        Files.objects.filter(pk__in=file_list).delete()
+        return render(request, self.template_name, context)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        object_list = self.get_queryset()
+        context = super().get_context_data(**kwargs, object_list=object_list)
+        return context
 
 
 @login_required(login_url='home')
