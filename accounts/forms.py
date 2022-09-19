21d87b1f4cffa89953d262e379bdde2970040814
Korets Roman <vip.korets@ukr.net>
2022-09-19 13:40:49 +0300

    add profile change password, template fix bag

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
+
 
 from captcha.fields import CaptchaField
 
@@ -24,19 +26,24 @@ class LoginUserForm(AuthenticationForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
-class UpdateUserForm(UserCreationForm):
+class UpdateUserForm(forms.Form):
     username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    # password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    # password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class PasswordProfileForm(forms.Form):
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class PasswordProfileFormBasic(PasswordChangeForm):
+
+    old_password = forms.CharField(label='Old password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    new_password1 = forms.CharField(label='New password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    new_password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
-        model = User
         fields = [
-            'username', 'email',
+            'old_password', 'new_password1', 'new_password2'
         ]
-
-
-# class ContactForm(forms.Form):
-#     subject = forms.CharField(label='Subject', widget=forms.TextInput(attrs={'class': 'form-control'}))
-#     content = forms.CharField(label='Text', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
