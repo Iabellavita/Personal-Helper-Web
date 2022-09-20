22e92b51260211d69f1aa5d69266ff98e84fde63
Iabellavita <vladganggg@gmail.com>
2022-09-20 09:21:58 +0300

    final

--- Personal_Helper/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import *
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-]
